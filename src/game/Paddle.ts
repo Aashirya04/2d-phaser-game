903d670c7edd8a1736e619135bf2cdd18e0cb532
Aashirya04 <aachandaka@gmail.com>
2023-12-22 21:28:43 +0530

    updated

--- src/game/Paddle.ts
+++ src/game/Paddle.ts
@@ -1,64 +1,75 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
-export default class Paddle extends Phaser.Physics.Matter.Image
-{
-	private ball?: Phaser.Physics.Matter.Image
+export default class Paddle extends Phaser.Physics.Matter.Image {
+    private ball?: Phaser.Physics.Matter.Image;
 
-	constructor(world: Phaser.Physics.Matter.World, x: number, y: number, texture: string, config?: Phaser.Types.Physics.Matter.MatterBodyConfig)
-	{
-		super(world, x, y, texture, undefined, config)
+    constructor(
+        world: Phaser.Physics.Matter.World,
+        x: number,
+        y: number,
+        texture: string,
+        config?: Phaser.Types.Physics.Matter.MatterBodyConfig
+    ) {
+        super(world, x, y, texture, undefined, config);
 
-		world.scene.add.existing(this)
-	}
+        world.scene.add.existing(this);
+    }
 
-	attachBall(ball: Phaser.Physics.Matter.Image)
-	{
-		this.ball = ball
-		this.ball.x = this.x
-		this.ball.y = this.y - (this.height * 0.5) - (ball.height * 0.5)
+    attachBall(ball: Phaser.Physics.Matter.Image) {
+        this.ball = ball;
+        this.ball.x = this.x;
+        this.ball.y = this.y - (this.height * 0.5) - (ball.height * 0.5);
 
-		this.ball.setVelocity(0, 0)
-	}
+        this.ball.setVelocity(0, 0);
+    }
 
-	launch()
-	{
-		if (!this.ball)
-		{
-			return
-		}
+    launch() {
+        if (!this.ball) {
+            return;
+        }
 
-		const { width, height } = this.scene.scale
-		const x = width * 0.5
-		const y = height * 0.5
+        const { width, height } = this.scene.scale;
+        const x = width * 0.5;
+        const y = height * 0.5;
 
-		const vx = x - this.ball.x
-		const vy = y - this.ball.y
+        const vx = x - this.ball.x;
+        const vy = y - this.ball.y;
 
-		const vec = new Phaser.Math.Vector2(vx, vy)
-			.normalize()
-			.scale(8)
+        const vec = new Phaser.Math.Vector2(vx, vy)
+            .normalize()
+            .scale(8);
 
-		this.ball.setVelocity(vec.x, vec.y)
+        this.ball.setVelocity(vec.x, vec.y);
 
-		this.ball = undefined
-	}
+        this.ball = undefined;
+    }
 
-	update(cursors: Phaser.Types.Input.Keyboard.CursorKeys)
-	{
-		const speed = 10
-		if (cursors.left?.isDown)
-		{
-			this.x -= speed
-			
-		}
-		else if (cursors.right?.isDown)
-		{
-			this.x += speed
-		}
+    update(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+        const speed = 10;
 
-		if (this.ball)
-		[
-			this.ball.x = this.x
-		]
-	}
+        if (cursors.left?.isDown) {
+            this.x -= speed;
+        }
+
+
+        if (cursors.right?.isDown) {
+            this.x += speed;
+        }
+
+        const { width } = this.scene.scale;
+        const halfPaddleWidth = this.width * 0.5;
+
+        if (this.x - halfPaddleWidth < 0) {
+            this.x = halfPaddleWidth;
+        }
+
+        if (this.x + halfPaddleWidth > width) {
+            this.x = width - halfPaddleWidth;
+        }
+
+
+        if (this.ball) {
+            this.ball.x = this.x;
+        }
+    }
 }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,135 +1,135 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
+import Paddle from '../game/Paddle';
 
-import Paddle from '../game/Paddle'
+const colors = [0xff0000, 0x00ff00, 0x0000ff];
 
-const colors = [
-	0xff0000,
-	0x00ff00,
-	0x0000ff
-]
+export default class Game extends Phaser.Scene {
+    private paddle!: Paddle;
+    private ball!: Phaser.Physics.Matter.Image;
+    private livesLabel!: Phaser.GameObjects.Text;
+    private lives = 3;
+    private score = 0;
+    private scoreLabel!: Phaser.GameObjects.Text;
+    private blocks: Phaser.Physics.Matter.Sprite[] = [];
+    private gameStarted = false;
 
-export default class Game extends Phaser.Scene
-{
-	private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-	private paddle!: Paddle
-	private ball!: Phaser.Physics.Matter.Image
+    constructor() {
+        super('game');
+    }
 
-	private livesLabel!: Phaser.GameObjects.Text
-	private lives = 3
+    init() {
+        this.lives = 3;
+        this.score = 0;
+        this.gameStarted = false;
+    }
 
-	private blocks: Phaser.Physics.Matter.Sprite[] = []
+    create() {
+        const { width, height } = this.scale;
 
-	constructor()
-	{
-		super('game')
-	}
+        const map = this.make.tilemap({ key: 'level1' });
+        const tileset = map.addTilesetImage('block', 'block');
 
-	init()
-	{
-		this.cursors = this.input.keyboard.createCursorKeys()
+        map.createLayer('Level', tileset);
 
-		this.lives = 3
-	}
+        this.blocks = map
+            .createFromTiles(1, 0, { key: 'block' })
+            .map(go => {
+                go.x += go.width * 0.5;
+                go.y += go.height * 0.5;
 
-	create()
-	{
-		const { width, height } = this.scale
+                const block = this.matter.add.gameObject(go, { isStatic: true }) as Phaser.Physics.Matter.Sprite;
+                block.setData('type', 'block');
+                block.setTint(colors[Phaser.Math.Between(0, colors.length - 1)]);
+                return block;
+            });
 
-		const map = this.make.tilemap({ key: 'level1' })
-		const tileset = map.addTilesetImage('block', 'block')
+        this.ball = this.matter.add.image(400, 300, 'ball', undefined, {
+            circleRadius: 12,
+        });
 
-		map.createStaticLayer('Level', tileset)
+        const body = this.ball.body as MatterJS.BodyType;
+        this.matter.body.setInertia(body, Infinity);
+        this.ball.setFriction(0, 0);
+        this.ball.setBounce(1);
 
-		this.blocks = map.createFromTiles(1, 0, { key: 'block' })
-			.map(go => {
-				go.x += go.width * 0.5
-				go.y += go.height * 0.5
+        this.paddle = new Paddle(this.matter.world, width * 0.5, height * 0.9, 'paddle', {
+            isStatic: true,
+            chamfer: {
+                radius: 15,
+            },
+        });
 
-				const block = this.matter.add.gameObject(go, { isStatic: true }) as Phaser.Physics.Matter.Sprite
-				block.setData('type', 'block')
-				block.setTint(colors[Phaser.Math.Between(0, colors.length - 1)])
-				return block 
-			})
+        this.paddle.attachBall(this.ball);
 
-		this.ball = this.matter.add.image(400, 300, 'ball', undefined, {
-			circleRadius: 12
-		})
+        this.livesLabel = this.add.text(10, 10, `Lives: ${this.lives}`, {
+            fontSize: '24px',
+        });
 
-		const body = this.ball.body as MatterJS.BodyType
-		this.matter.body.setInertia(body, Infinity)
-		this.ball.setFriction(0, 0)
-		this.ball.setBounce(1)
+        this.scoreLabel = this.add.text(width - 10, 10, `Score: ${this.score}`, {
+            fontSize: '24px',
+            align: 'right',
+        }).setOrigin(1, 0);
 
-		this.paddle = new Paddle(this.matter.world, width * 0.5, height * 0.9, 'paddle', {
-			isStatic: true,
-			chamfer: {
-				radius: 15
-			}
-		})
+        this.ball.setOnCollide(this.handleBallCollide.bind(this));
+        this.input.on('pointerdown', this.startGame.bind(this));
+        this.input.on('pointermove', this.handleTouchMove.bind(this));
+    }
 
-		this.paddle.attachBall(this.ball)
+    private startGame() {
+        if (!this.gameStarted) {
+            this.gameStarted = true;
+            this.paddle.launch();
+        }
+    }
 
-		this.livesLabel = this.add.text(10, 10, `Lives: ${this.lives}`, {
-			fontSize: 24
-		})
+    private handleTouchMove(pointer: Phaser.Input.Pointer) {
+        if (this.gameStarted) {
+            this.paddle.x = Phaser.Math.Clamp(pointer.x, this.paddle.width / 2, this.scale.width - this.paddle.width / 2);
+        }
+    }
 
-		this.ball.setOnCollide(this.handleBallCollide.bind(this))
-	}
+    private handleBallCollide(data: Phaser.Types.Physics.Matter.MatterCollisionData) {
+        const { bodyA } = data;
 
-	private handleBallCollide(data: Phaser.Types.Physics.Matter.MatterCollisionData)
-	{
-		const { bodyA } = data
+        if (!bodyA.gameObject) {
+            return;
+        }
 
-		if (!bodyA.gameObject)
-		{
-			return
-		}
+        const goA = bodyA.gameObject as Phaser.GameObjects.GameObject;
 
-		const goA = bodyA.gameObject as Phaser.GameObjects.GameObject
+        if (goA.getData('type') !== 'block') {
+            return;
+        }
 
-		if (goA.getData('type') !== 'block')
-		{
-			return
-		}
+        const idx = this.blocks.findIndex(block => block === goA);
+        if (idx >= 0) {
+            this.blocks.splice(idx, 1);
+        }
 
-		const idx = this.blocks.findIndex(block => block === goA)
-		if (idx >= 0)
-		{
-			this.blocks.splice(idx, 1)
-		}
+        this.sound.play('tone1');
+        goA.destroy(true);
 
-		this.sound.play('tone1')
-		goA.destroy(true)
+        this.score += 1;
+        this.scoreLabel.text = `Score: ${this.score}`;
 
-		if (this.blocks.length <= 0)
-		{
-			this.scene.start('game-over', { title: 'You Win', color: '#e1eb34' })
-		}
-	}
+        if (this.blocks.length <= 0) {
+            this.scene.start('game-over', { title: 'You Win', color: '#e1eb34', score: this.score });
+        }
+    }
 
-	update(t: number, dt: number)
-	{
-		if (this.ball.y > this.scale.height + 100)
-		{
-			--this.lives
-			this.livesLabel.text = `Lives: ${this.lives}`
+    update(t: number, dt: number) {
+        if (this.ball.y > this.scale.height + 100) {
+            --this.lives;
+            this.livesLabel.text = `Lives: ${this.lives}`;
 
-			if (this.lives <= 0)
-			{
-				this.scene.start('game-over', { title: 'Game Over' })
-				return
-			}
+            if (this.lives <= 0) {
+                this.scene.start('game-over', { title: 'Game Over', score: this.score });
+                return;
+            }
 
-			this.paddle.attachBall(this.ball)
-			return
-		}
-
-		const spaceJustDown = Phaser.Input.Keyboard.JustDown(this.cursors.space!)
-		if (spaceJustDown)
-		{
-			this.paddle.launch()
-		}
-
-		this.paddle.update(this.cursors)
-	}
+            this.paddle.attachBall(this.ball);
+            this.gameStarted = false;
+            return;
+        }
+    }
 }
